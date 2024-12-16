7d4490e6f160e5d840701fe0568216f6790bce85
Glenn Latomme <glenn.latomme@arcelormittal.com>
2024-12-16 15:54:14 +0100

    Initial commit

---
+++ apps/nestjs-nx-worker-thread-example/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'nestjs-nx-worker-thread-example',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nestjs-nx-worker-thread-example',
+};
